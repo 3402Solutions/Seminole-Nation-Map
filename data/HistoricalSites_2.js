17c5ea7fe008ed3b1cdf34775bcd2f168ed263e1
3402Solutions <66091895+3402Solutions@users.noreply.github.com>
2020-06-11 09:54:51 -0500

    Add files via upload

---
+++ data/HistoricalSites_2.js
@@ -0,0 +1,23 @@
+var json_HistoricalSites_2 = {
+"type": "FeatureCollection",
+"name": "HistoricalSites_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Location": "Alice Brown House", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.528611, 34.946111 ] } },
+{ "type": "Feature", "properties": { "Location": "Jackson Brown House", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.4904, 35.148 ] } },
+{ "type": "Feature", "properties": { "Location": "Silas L. Brown House", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.486667, 35.158056 ] } },
+{ "type": "Feature", "properties": { "Location": "W.E. Grisso Mansion", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.6645, 35.2471 ] } },
+{ "type": "Feature", "properties": { "Location": "Home Stake Oil and Gas Company Building", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.6678, 35.2243 ] } },
+{ "type": "Feature", "properties": { "Location": "Hotel Aldridge", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.4913, 35.1568 ] } },
+{ "type": "Feature", "properties": { "Location": "J. Coody Johnson Building", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.4912, 35.1596 ] } },
+{ "type": "Feature", "properties": { "Location": "Konawa Armory", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.7513, 34.9641 ] } },
+{ "type": "Feature", "properties": { "Location": "Mekasukey Academy", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.721667, 35.190278 ] } },
+{ "type": "Feature", "properties": { "Location": "Rosenwald Hall", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.5988, 35.1742 ] } },
+{ "type": "Feature", "properties": { "Location": "Seminole County Courthouse", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.490833, 35.158056 ] } },
+{ "type": "Feature", "properties": { "Location": "Seminole Municipal Building", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.6687, 35.2268 ] } },
+{ "type": "Feature", "properties": { "Location": "Seminole Whipping Tree", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.4912, 35.1581 ] } },
+{ "type": "Feature", "properties": { "Location": "Sinclair Loading Rack", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.6389, 35.2159 ] } },
+{ "type": "Feature", "properties": { "Location": "Strother Memorial Chapel", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.6855, 35.234 ] } },
+{ "type": "Feature", "properties": { "Location": "Wewoka Switch and Side Tracks", "Source": "Source: Wikipedia article National Register of Hi*" }, "geometry": { "type": "Point", "coordinates": [ -96.4911, 35.1609 ] } }
+]
+}
